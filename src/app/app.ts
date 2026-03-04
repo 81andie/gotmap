a80f7def51e3a224af0b1ebfcc4192080cdda3be
81andie <133395577+81andie@users.noreply.github.com>
2026-03-04 20:11:46 +0100

    añadiendo el mapa, un minimap y el sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { Maps } from "./components/map/map";
+import { Sidenav } from "./components/sidenav/sidenav";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Maps, Sidenav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App  implements OnInit{
   protected readonly title = signal('gotMap');
+
+  ngOnInit():void{
+     initFlowbite();
+}
 }
--- src/app/components/map/map.ts
+++ src/app/components/map/map.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Map, NavigationControl } from 'maplibre-gl';
+import MinimapControl from "maplibregl-minimap";
+
 
 @Component({
   selector: 'app-map',
@@ -6,4 +9,57 @@ import { Component } from '@angular/core';
   templateUrl: './map.html',
   styleUrl: './map.css',
 })
-export class Map {}
+export class Maps implements OnInit {
+
+  ngOnInit(): void {
+
+    this.getMap()
+
+  }
+
+   getMap() {
+
+    let nav = new NavigationControl();
+
+    var map = new Map({
+      container: 'map',
+      style: 'https://api.maptiler.com/maps/hybrid-v4/style.json?key=bZ943IENWwexU3umotpo', // stylesheet location
+      center: [-3.5, 40], // starting position [lng, lat]
+      zoom: 3 // starting zoom,
+
+    });
+
+     const miniMap = new Map({
+      container: 'minimap',
+      style: 'https://api.maptiler.com/maps/hybrid-v4/style.json?key=bZ943IENWwexU3umotpo',
+      center: [-74.5, 40],
+      zoom: 4,
+      attributionControl: false
+
+    });
+
+
+      map.on('load', () => {
+      miniMap.setCenter(map.getCenter());
+      miniMap.setZoom(Math.max(map.getZoom() - 5, 0));
+    });
+
+    map.on('move', () => {
+      miniMap.setCenter(map.getCenter());
+      miniMap.setZoom(Math.max(map.getZoom() - 5, 0));
+    });
+
+     map.addControl(nav, 'top-left');
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
